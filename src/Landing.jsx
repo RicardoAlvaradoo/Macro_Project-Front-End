b693df16a67084a6afd24082fe5dcb7cfce820cc
rickybua <rickybua@bu.edu>
2024-06-03 06:58:46 -0400

    api json loads properly

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -1,56 +1,103 @@
 import React, { useState, useEffect } from 'react';
 import './landing.css'
-
-
-function Landing(){
-    console.log("Arrive in function");
-    const [message, setMessage] = useState('No Data Yet');
-  
-    function restaurant_data() {
-    console.log("Restaurant Data Called");
-    const csrftoken = document.querySelector('[label = "min" name=csrfmiddlewaretoken]').value;
-    const options = {
-        method:"POST", 
+function Landing(props){
+  console.log("RAN")
+  const [message, setMessage] = useState(<h1>No data yet</h1>);
+  const [user, setUser] = useState(props.user)
+  async function restaurant_data() {
+      console.log("Restaurant Data Called ", user);
+      //const csrftoken =  getCookie('csrftoken');
+      const options = {
+          method:"POST", 
+          mode: 'cors',
+          headers: {
+              'Content-Type':'application/x-www-form-urlencoded',
+              
+          },
+          body:JSON.stringify({user}),
+          
+      };
+      await fetch('http://127.0.0.1:8000/orders', options).then(response => response.json().then(result => {
+      result = JSON.stringify(result)
+      result = JSON.parse(result);
+      result = JSON.parse(result);
+      console.log("Received data value: ",result.data.rest);   
+      let orders = result.data.rest;
+      const arrayDataItems = orders.map( items=> 
         
-        headers: {
-            'Content-Type':'application/json',
-            'X-CSRFToken': csrftoken,
-        },
-        body:JSON.stringify(data)
-        
-    };
-    fetch('http://127.0.0.1:8000/test', options).then(response => {
-          setMessage(response.data.message);
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    };
-  
+        <p>{items[1][0]} {items[1][1]} {items[1][2]} {items[1][3]}</p>
+         
+      )
+        setMessage( arrayDataItems );
+          }))
+          .catch(error => {
+            console.log(error);
+          });
+      };
+     
+    function handleSubmit(e){
+      
+      e.preventDefault();
+      restaurant_data();
+    }
     return (
       <>
-      <h1>{message}</h1>
+      {message}
       <form >
         <div>
-        <label >Cal Min:<input  name="cal_min"/></label>
-        <label >Cal Max:<input placeholder = "cal_max" name="cal_max"/></label>
+        <label >Cal Min:
+          <input  
+          name="user[cal_min]" 
+          onChange={e => setUser({ ...user, cal_min: e.target.value })}/>
+        </label>
+        <label >Cal Max:
+          <input  
+          name="user[cal_max]"  
+          onChange={e => setUser({ ...user, cal_max: e.target.value })}/>
+        </label>
+        </div>
+        
+        <div>
+        <label >Protein Min:
+          <input  
+          name="user[pro_min]" 
+          onChange={e => setUser({ ...user,pro_min: e.target.value })}/>
+          </label>
+        <label>Protein Max:
+          <input  
+          name="user[pro_max]"
+          onChange={e => setUser({ ...user, pro_max: e.target.value })}/>
+          </label>
         </div>
         <div>
-        <label >Protein Min:<input  name="pro_min"/></label>
-        <label>Protein Max:<input  name="pro_max"/></label>
+        <label >Carbohydrate Min:
+          <input 
+          name="user[carb_min]"
+          onChange={e => setUser({ ...user, carb_min: e.target.value })}/>
+          </label>
+        <label >Carbohydrate Max:
+          <input  
+          name="user[carb_max]"
+          onChange={e => setUser({ ...user, carb_max: e.target.value })}/>
+          </label>
         </div>
+        
         <div>
-        <label >Carbohydrate Min:<input  name="carb_min"/></label>
-        <label >Carbohydrate Max:<input  name="carb_max"/></label>
+        <label >Fat Min:<input  
+        name="user[fat_min]"
+        onChange={e => setUser({ ...user, fat_min: e.target.value })}/>
+        </label>
+        <label >Fat Max:
+          <input  
+          name="user[fat_max]"
+          onChange={e => setUser({ ...user, fat_max: e.target.value })}/>
+          </label>
         </div>
-        <div>
-        <label >Fat Min:<input  name="fat_min"/></label>
-        <label >Fat Max:<input  name="fat_max"/></label>
-        </div>
-        <button onClick={restaurant_data()} type="submit" label = "min" name="macros" values="Enter Macros">Search</button>
+        <button onClick={handleSubmit} type="submit" label = "min" name="macros" values="Enter Macros">Search</button>
       </form>
       </>
     );
   }
+  
   
   export default Landing;
