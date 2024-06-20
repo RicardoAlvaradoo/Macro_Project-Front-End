fb67ca23fed446a1b8c197778a7d4c928584ffe3
rickybua <rickybua@bu.edu>
2024-06-20 18:43:28 -0400

    styling

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -16,45 +16,23 @@ function Landing(props) {
   //input
 
   const [login_user, loginUser] = useState();
-  let user = useRef(props.user);
+
   //slider logic
 
   //distance
   const [dist, set_dist] = useState(5);
   const MAX = 10;
 
-  const getBackgroundSize = () => { 
-    return { backgroundSize: `${(dist * 100) / MAX}% 100%` }; }; 
-  const [minCal, set_minCal] = useState(100);
-  const [maxCal, set_maxCal] = useState(900);
-  //protein
-  const [minPro, set_minPro] = useState(20);
-  const [maxPro, set_maxPro] = useState(80);
-  //carb
-  const [minCarb, set_minCarb] = useState(30);
-  const [maxCarb, set_maxCarb] = useState(120);
-  //fat
-  const [minFat, set_minFat] = useState(20);
-  const [maxFat, set_maxFat] = useState(80);
-
+  const getBackgroundSize = () => {
+    return { backgroundSize: `${(dist * 100) / MAX}% 100%` };
+  };
+  const [user, setUser] = useState(props.user)
 
 
 
   async function restaurant_data() {
 
 
-    user = {
-      ...user,
-      cal_min: minCal,
-      cal_max: maxCal,
-      pro_min: minPro,
-      pro_max: maxPro,
-      carb_min: minCarb,
-      carb_max: maxCarb,
-      fat_min: minFat,
-      fat_max: maxFat,
-      distance: dist,
-    };
 
     console.log("Restaurant Data Called ", user.cal_max);
 
@@ -99,126 +77,98 @@ function Landing(props) {
     <>
       <Nav />
       <div id="Landing">
-      
+
         <div className='row'>
-        <div className='column'>
-        <div className='Data'>
+          <div className='column'>
+            <div className='Data'>
 
-{message}
+              <span>{message}</span>
 
-</div>
-        <form>
-          <div className='Container'>
-            <span>Distance: {dist}</span>
-            <div className='sliderBox'>
-            <input type="range"
-            min="0" 
-            max={10}
-            onChange={(e) => set_dist(e.target.value)} 
-            style={getBackgroundSize()} value={dist} 
-  />
-           
+            </div>
+            <form >
+              <div className='Container'>
+                <label>Distance</label>
+                <div className='sliderBox'>
+                  <input type="range"
+                    min="0"
+                    max={10}
+                    onChange={(e) => set_dist(e.target.value)}
+                    style={getBackgroundSize()} value={dist}
+                  />
+
+                </div>
+              </div>
+              <div className='Container'>
+                <label >Calories   </label>
+                <input
+                  name="user[cal_min]"
+                  onChange={e => setUser({ ...user, cal_min: e.target.value })}
+                  className='input-data' />
+
+                <span className='semi'>-</span>
+                <input
+                  name="user[cal_max]"
+                  onChange={e => setUser({ ...user, cal_max: e.target.value })}
+                  className='input-data' />
+
+              </div>
+
+              <div className='Container'>
+                <label >Protein  </label>
+                <input
+                  name="user[pro_min]"
+                  onChange={e => setUser({ ...user, pro_min: e.target.value })}
+                  className='input-data' />
+                <span className='semi'>-</span>
+
+                <input
+                  name="user[pro_max]"
+                  onChange={e => setUser({ ...user, pro_max: e.target.value })}
+                  className='input-data' />
+
+              </div>
+              <div className='Container'>
+                <label >Carbohydrates  </label>
+
+                <input
+                  name="user[carb_min]"
+                  onChange={e => setUser({ ...user, carb_min: e.target.value })}
+                  className='input-data' />
+                <span className='semi'>-</span>
+                <input
+                  name="user[carb_max]"
+                  onChange={e => setUser({ ...user, carb_max: e.target.value })}
+                  className='input-data' />
+
+              </div>
+
+              <div className='Container'>
+                <label >Fat </label>
+                <input
+                  name="user[fat_min]"
+                  onChange={e => setUser({ ...user, fat_min: e.target.value })}
+                  className='input-data' />
+                <span className='semi'>-</span>
+
+                <input
+                  name="user[fat_max]"
+                  onChange={e => setUser({ ...user, fat_max: e.target.value })}
+                  className='input-data' />
+
+              </div>
+              <button onClick={handleSubmit} id="search" type="submit" label="min" name="macros" values="Enter Macros">Search</button>
+            </form>
+            <div id='Log-In'>
+            <a href='Login'>Log in</a><span> to view previous orders and save your profile.</span>
             </div>
           </div>
 
-          <div className='Container'>
-            <div className='label'>
-              <span>Calories:  {minCal} {maxCal}</span>
-            </div>
-            <div className='sliderBox'>
-              <MultiRangeSlider
-                min={0}
-                max={1000}
-                label='false'
-                ruler='false'
+          <div className='column'>
 
-                minValue={minCal}
-                maxValue={maxCal}
-                onInput={(e) => {
-                  set_minCal(e.minValue);
-                  set_maxCal(e.maxValue);
-
-
-                }}
-              />
-            </div>
-          </div>
-          <div className='Container'>
-            <div className='label'>
-
-              <span>Protein</span>
-            </div>
-            <div className='sliderBox'>
-              <MultiRangeSlider
-                min={0}
-                max={100}
-                label='false'
-                ruler='false'
-
-                minValue={minPro}
-                maxValue={maxPro}
-                onInput={(e) => {
-                  set_minPro(e.minValue);
-                  set_maxPro(e.maxValue);
-
-
-                }}
-              />
-            </div>
-          </div>
-          <div className='Container'>
-            <div className='label'>
-              <span>Carbohydrates:</span>
-            </div>
-            <div className='sliderBox'>
-              <MultiRangeSlider
-                min={0}
-                max={150}
-                label='false'
-                ruler='false'
-
-                minValue={minCarb}
-                maxValue={maxCarb}
-                onInput={(e) => {
-                  set_minCarb(e.minValue);
-                  set_maxCarb(e.maxValue);
-
-                }}
-              />
-            </div>
+            <img src={Image} id='logo-map' />
           </div>
 
-          <div className='Container'>
-            <div className='label'>
-              <span>Fat:</span>
-            </div>
-            <div className='sliderBox'>
-              <MultiRangeSlider
-                min={0}
-                max={100}
-                label='false'
-                ruler='false'
-
-                minValue={minFat}
-                maxValue={maxFat}
-                onInput={(e) => {
-                  set_minFat(e.minValue);
-                  set_maxFat(e.maxValue);
-
-                }}
-              />
-            </div>
-          </div>
-          <button onClick={handleSubmit} id="search" type="submit" label="min" name="macros" values="Enter Macros">Search</button>
-        </form>
         </div>
-        
-        <div className='column'>
-          
-        <img src={Image} id='logo-map'/>
-        </div>
-      
-      </div>
       </div>
     </>
   );
